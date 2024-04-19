3d063249075baff6a88ba609aa86a7a947cab289
mackwick <mackenziewicker@gmail.com>
2024-04-19 11:21:24 -0400

    show page filled out

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1,14 +1,10 @@
-import Pause from "../components/Pause";
-import { useLoaderData } from "react-router-dom";
+// import Pause from "../components/Pause";
+// import { useLoaderData } from "react-router-dom";
 
 function Index() {
-  const allPauses: any = useLoaderData();
-
   return (
     <div className="index">
-      {allPauses.map((pause: any, id: number) => {
-        <Pause pause={pause} key={id} />;
-      })}
+      <h1>Index</h1>
     </div>
   );
 }
--- src/pages/Show.tsx
+++ src/pages/Show.tsx
@@ -2,10 +2,24 @@ import { useLoaderData } from "react-router-dom";
 
 function Show() {
   const aPause: any = useLoaderData();
-  // const id: number = aPause.id;
+  const date: string = aPause.date.split("T")[0];
+  const displayDate: string =
+    date.split("-")[1] + "." + date.split("-")[2] + "." + date.split("-")[0];
   return (
     <div className="show">
-      <h1>{aPause.length}</h1>
+      <h1>
+        {aPause.length} minute meditation on {displayDate}
+      </h1>
+      <h2>
+        <b>Feelings before: </b>
+        {aPause.before}
+      </h2>
+      <h2>
+        <b>Feelings after: </b>
+        {aPause.after}
+      </h2>
+      <h3>What came up during this session:</h3>
+      <p>{aPause.thoughts}</p>
     </div>
   );
 }
