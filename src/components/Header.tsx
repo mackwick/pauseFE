e4e2f0d61313cc6dc52878d83bb2cbc1078689ac
mackwick <mackenziewicker@gmail.com>
2024-04-24 19:44:53 -0400

    beta hamburg menu done, gotta refine tomorrow

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,15 +1,33 @@
 import { Link } from "react-router-dom";
 
 function Header() {
+  // const hamMenu = document.querySelector("hamburger-menu");
+  // const offScreenMenu = document.querySelector("offscreen-menu");
+  // hamMenu?.addEventListener("click", () => {
+  //   hamMenu?.classList.toggle("active");
+  //   offScreenMenu?.classList.toggle("active");
+  // });
+
   return (
     <div className="header">
       <nav>
-        <Link to="/" className="link">
-          Home
-        </Link>
-        <Link to="/index" className="link">
-          All Pauses
-        </Link>
+        <div className="offscreen-menu">
+          <Link to="/" className="link">
+            Home
+          </Link>
+          <Link to="/index" className="link">
+            All Pauses
+          </Link>
+        </div>
+
+        <div className="hamburger-menu">
+          <label htmlFor="ham-toggle">
+            <span></span>
+            <span></span>
+            <span></span>
+          </label>
+          <input type="checkbox" id="ham-toggle" />
+        </div>
       </nav>
       <Link to="/" className="link">
         <h1>Pause</h1>
