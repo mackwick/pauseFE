8a8d254d2580c429699e481a08d304afe32403f0
mackwick <mackenziewicker@gmail.com>
2024-04-26 16:24:08 -0400

    a few small tweaks, MVP achieved

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -9,9 +9,12 @@ const Index: React.FC = () => {
   const [currentPage, setCurrentPage] = useState(1);
   const postPerPage = 12;
 
+  const sortedPauses: any = allPauses.sort((a: any, b: any) => {
+    return b.id - a.id;
+  });
   const lastPostIndex = currentPage * postPerPage;
   const firstPostIndex = lastPostIndex - postPerPage;
-  const currentPosts: any = allPauses.slice(firstPostIndex, lastPostIndex);
+  const currentPosts: any = sortedPauses.slice(firstPostIndex, lastPostIndex);
 
   return (
     <div>
